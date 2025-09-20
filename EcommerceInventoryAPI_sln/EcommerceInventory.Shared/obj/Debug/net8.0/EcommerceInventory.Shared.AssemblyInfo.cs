24f9ad74842712a4723cb9b7ead4146027da119f
riyad102hossain <rh005323@gmail.com>
2025-09-20 17:38:46 +0600

    Add project files.

--- EcommerceInventoryAPI_sln/EcommerceInventory.Shared/obj/Debug/net8.0/EcommerceInventory.Shared.AssemblyInfo.cs
+++ EcommerceInventoryAPI_sln/EcommerceInventory.Shared/obj/Debug/net8.0/EcommerceInventory.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceInventory.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9718c86366b0fc1c4ba5f6af9f96aa13d5346f3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceInventory.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceInventory.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
